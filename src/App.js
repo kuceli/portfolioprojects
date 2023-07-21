f59237bd70a62db409224a308dd80de8d8317bbf
Englama Kuceli Susan <kucelienglama@ymail.com>
2023-07-21 22:54:22 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import ClonedSites from "./pages/ClonedSites";
-import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
-import MiniProjects from "./pages/MiniProjects";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/clonedsites" element={<ClonedSites />} />
-        <Route path="/miniprojects" element={<MiniProjects />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <ClonedSites />
     </div>
   );
 }
